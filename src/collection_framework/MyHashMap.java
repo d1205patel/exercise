ff61d9ce93ef04cfab1c7a965994c8f231c04d82
Darshan <darshan1205patel@gmail.com>
2020-04-08 16:26:45 +0530

    Implementation of Map without the help of AbstractMap

--- src/collection_framework/MyHashMap.java
+++ src/collection_framework/MyHashMap.java
@@ -2,7 +2,7 @@ package collection_framework;
 
 import java.util.*;
 
-public class MyHashMap<K,V> extends AbstractMap<K,V> implements Map<K,V>{
+public class MyHashMap<K,V> implements Map<K,V>{
 
     private static int MAX_CAP = 1<<30;
     private static int DEFAULT_CAP = 16;
@@ -11,8 +11,13 @@ public class MyHashMap<K,V> extends AbstractMap<K,V> implements Map<K,V>{
     private int size;
     private int threshold;
     private float loadFactor;
+    private int modCount;
 
     private Node<K,V>[] table;
+
+    private Set<Entry<K,V>> es;
+    private Set<K> ks;
+    private Collection<V> values;
 
     //<----------------------------------------- Constructors ------------------------------------------>//
 
@@ -44,13 +49,24 @@ public class MyHashMap<K,V> extends AbstractMap<K,V> implements Map<K,V>{
     }
 
     @Override
-    public boolean remove(Object key, Object value) {
-        return Objects.equals(removeNode(key,value,true),value);
+    public void putAll(Map<? extends K, ? extends V> m) {
+        if(m!=null) {
+            for (Entry<? extends K, ? extends V> entry : m.entrySet()) {
+                putVal(entry.getKey(), entry.getValue());
+            }
+        }
     }
 
     @Override
+    public boolean remove(Object key, Object value) {
+        Node<K,V> removedNode = removeNode(key,value,true);
+        return removedNode!=null && Objects.equals(removedNode.value,value);
+}
+
+    @Override
     public V remove(Object key) {
-        return removeNode(key,null,false);
+        Node<K,V> removedNode = removeNode(key,null,false);
+        return removedNode==null ? null : removedNode.value;
     }
 
     @Override
@@ -71,8 +87,7 @@ public class MyHashMap<K,V> extends AbstractMap<K,V> implements Map<K,V>{
     @Override
     public void clear() {
         if(table!=null) {
-            for(int i=0;i<table.length;i++)
-                table[i] = null;
+            Arrays.fill(table,null);
         }
     }
 
@@ -90,8 +105,7 @@ public class MyHashMap<K,V> extends AbstractMap<K,V> implements Map<K,V>{
     @Override
     public boolean containsValue(Object value) {
         if(table!=null) {
-            for(int i=0;i<table.length;i++) {
-                Node<K,V> node = table[i];
+            for(Node<K,V> node:table) {
                 while(node!=null) {
                     if(Objects.equals(value,node.value)) {
                         return true;
@@ -114,8 +128,379 @@ public class MyHashMap<K,V> extends AbstractMap<K,V> implements Map<K,V>{
     }
 
     @Override
+    public boolean equals(Object o) {
+        if(o==this) {
+            return true;
+        }
+        if(o instanceof Map<?,?>) {
+            Map<?,?> map = (Map<?,?>)o;
+            Set<Entry<K,V>>  entrySet = entrySet();
+            for(Entry<?,?> entry: ((Map<?, ?>) o).entrySet()) {
+                if(!entrySet.contains(entry)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return entrySet().hashCode();
+    }
+
+    //<------------------------- Methods that returns view(mutable) of HashMap ---------------------------->//
+
+    @Override
     public Set<Entry<K, V>> entrySet() {
-        return null;
+        return es==null ? es = new EntrySet() : es;
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return ks==null ? ks = new KeySet() : ks;
+    }
+
+    @Override
+    public Collection<V> values() {
+        return values==null ? values = new Values() : values;
+    }
+
+    //<----------------------------------------- Views of HashMap----------------------------------------->//
+
+    private abstract class CommonHashMapOperation {
+        public int size() {
+            return size;
+        }
+        public boolean isEmpty() {
+            return size==0;
+        }
+        public void clear() {
+            MyHashMap.this.clear();
+        }
+        public boolean contains(Object o) {
+            throw new UnsupportedOperationException();
+        }
+        public boolean containsAll(Collection<?> c) {
+            for(Object o: c) {
+                if(!contains(o)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public boolean remove(Object o) {
+            throw new UnsupportedOperationException();
+        }
+        public boolean removeAll(Collection<?> c) {
+            boolean changed = false;
+            for(Object o:c) {
+                changed |= remove(o);
+            }
+            return changed;
+        }
+        public boolean retainAll(Collection<?> c) {
+            boolean changed = false;
+            for(Object o: c) {
+                if(!contains(o)) {
+                    changed |= remove(o);
+                }
+            }
+            return changed;
+        }
+    }
+
+
+    private class EntrySet extends CommonHashMapOperation implements Set<Entry<K,V>> {
+
+        @Override
+        public boolean contains(Object o) {
+            if(o instanceof Entry<?,?>) {
+                Entry<?,?> entry = (Entry<?,?>) o;
+                Object key = entry.getKey();
+                Object value = entry.getValue();
+                return Objects.equals(value,get(key));
+            }
+            return false;
+        }
+
+        @Override
+        public Iterator<Entry<K, V>> iterator() {
+            return new EntrySetItr();
+        }
+
+        @Override
+        public Object[] toArray() {
+            Object[] array = new Object[size];
+            int i = 0;
+            for(Entry<K,V> entry: entrySet()) {
+                array[i++] = entry;
+            }
+            return array;
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T> T[] toArray(T[] a) {
+            if(a.length < size) {
+                return (T[])toArray();
+            }
+            int i = 0;
+            for(Entry<K,V> entry: entrySet()) {
+                a[i++] = (T)entry;
+            }
+            if(i < size) {
+                a[i] = null;
+            }
+            return a;
+        }
+
+        @Override
+        public boolean add(Entry<K, V> kvEntry) {
+            K key = kvEntry.getKey();
+            V value = kvEntry.getValue();
+            if(Objects.equals(value,get(key))) {
+                return false;
+            }
+            putVal(key,value);
+            return true;
+        }
+
+        @Override
+        public boolean remove(Object o) {
+            if(o instanceof Entry<?,?>) {
+                Entry<?,?> entry = (Entry<?,?>)o;
+                return MyHashMap.this.remove(entry.getKey(),entry.getValue());
+            }
+            return false;
+        }
+
+        @Override
+        public boolean addAll(Collection<? extends Entry<K, V>> c) {
+            for(Entry<K,V> entry: c) {
+                putVal(entry.getKey(),entry.getValue());
+            }
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            int hashCode = 0;
+            for(Entry<K,V> entry:this) {
+                hashCode += entry.hashCode();
+            }
+            return hashCode;
+        }
+
+    }
+
+    private class KeySet extends CommonHashMapOperation implements Set<K> {
+
+        @Override
+        public boolean contains(Object o) {
+            return getNode(o)!=null;
+        }
+
+        @Override
+        public Iterator<K> iterator() {
+            return new KeyItr();
+        }
+
+        @Override
+        public Object[] toArray() {
+            Object[] array = new Object[size];
+            int i = 0;
+            for(Entry<K,V> entry: entrySet()) {
+                array[i++] = entry.getKey();
+            }
+            return array;
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T> T[] toArray(T[] a) {
+            if(a.length < size) {
+                return (T[])toArray();
+            }
+            int i = 0;
+            for(Entry<K,V> entry: entrySet()) {
+                a[i++] = (T)entry.getKey();
+            }
+            if(i < size) {
+                a[i] = null;
+            }
+            return a;
+        }
+
+        @Override
+        public boolean add(K k) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean remove(Object o) {
+            Node<K,V> removedNode = removeNode(o,null,false);
+            return removedNode!=null;
+        }
+
+        @Override
+        public boolean addAll(Collection<? extends K> c) {
+            throw new UnsupportedOperationException();
+        }
+
+    }
+
+    private class Values extends CommonHashMapOperation implements Collection<V> {
+
+        @Override
+        public boolean contains(Object o) {
+            return containsValue(o);
+        }
+
+        @Override
+        public Iterator<V> iterator() {
+            return new ValueItr();
+        }
+
+        @Override
+        public Object[] toArray() {
+            Object[] array = new Object[size];
+            int i = 0;
+            for(Entry<K,V> entry: entrySet()) {
+                array[i++] = entry.getValue();
+            }
+            return array;
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T> T[] toArray(T[] a) {
+            if(a.length < size) {
+                return (T[])toArray();
+            }
+            int i = 0;
+            for(Entry<K,V> entry: entrySet()) {
+                a[i++] = (T)entry.getValue();
+            }
+            if(i < size) {
+                a[i] = null;
+            }
+            return a;
+        }
+
+        @Override
+        public boolean add(V v) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean remove(Object o) {
+            Iterator<V> it = iterator();
+            while(it.hasNext()) {
+                V value = it.next();
+                if(Objects.equals(value,o)) {
+                    it.remove();
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        @Override
+        public boolean addAll(Collection<? extends V> c) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean removeAll(Collection<?> c) {
+            boolean changed = false;
+            for(Object o : c) {
+                Iterator<V> it = iterator();
+                while(it.hasNext()) {
+                    V value = it.next();
+                    if(Objects.equals(value,o)) {
+                        it.remove();
+                        changed = true;
+                    }
+                }
+            }
+            return changed;
+        }
+
+    }
+
+    //<------------------------------------------- Iterators --------------------------------------------->//
+
+    private class HashItr {
+        Node<K,V> currentNode,nextNode;
+        int expectedModCount;
+        int index;
+
+        HashItr() {
+            currentNode = null;
+            if(table!=null) {
+                int n = table.length ;
+                index = 0;
+                expectedModCount = modCount;
+                while(index < n && (nextNode=table[index++])==null);
+            }
+        }
+
+        public boolean hasNext() {
+            return nextNode!=null;
+        }
+
+        public Entry<K, V> nextNode() {
+            checkForConcurrentModification();
+            if(nextNode==null) {
+                throw new NoSuchElementException();
+            }
+            currentNode = nextNode;
+            if((nextNode = nextNode.next)==null) {
+                int n = table.length;
+                while(index < n && (nextNode=table[index++])==null);
+            }
+            return currentNode;
+        }
+
+        public void remove() {
+            checkForConcurrentModification();
+            if(currentNode==null) {
+                throw new IllegalStateException();
+            }
+            MyHashMap.this.remove(currentNode.key);
+            currentNode = null;
+            expectedModCount = modCount;
+        }
+
+        private void checkForConcurrentModification() {
+            if(expectedModCount != modCount) {
+                throw new ConcurrentModificationException();
+            }
+        }
+    }
+
+    private class EntrySetItr extends HashItr implements Iterator<Entry<K,V>> {
+        @Override
+        public Entry<K,V> next() {
+            return nextNode();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private class KeyItr extends HashItr implements Iterator<K> {
+        @Override
+        public K next() {
+            return (K)nextNode().getValue();
+        }
+    }
+
+    private class ValueItr extends HashItr implements Iterator<V> {
+        @Override
+        public V next() {
+            return nextNode().getValue();
+        }
     }
 
     //<--------------------------------- Private Helper methods --------------------------------------->//
@@ -128,10 +513,9 @@ public class MyHashMap<K,V> extends AbstractMap<K,V> implements Map<K,V>{
         int hash = hash(key);
         int index = hash & (n-1);
         if(table[index]==null) {
-            table[index] = new Node(hash,key,value,null);
+            table[index] = new Node<>(hash, key, value, null);
         } else {
-            Node<K,V> foundNode = null;
-            Node<K,V> prevNode = table[index];
+            Node<K,V> foundNode ,prevNode = table[index];
             if(hash==prevNode.hash && Objects.equals(key,prevNode.key)) {
                 foundNode = prevNode;
             } else {
@@ -147,21 +531,22 @@ public class MyHashMap<K,V> extends AbstractMap<K,V> implements Map<K,V>{
                 foundNode.value = value;
                 return oldValue;
             } else {
-                prevNode.next = new Node(hash,key,value,null);
+                prevNode.next = new Node<>(hash,key,value,null);
             }
         }
         if(++size>threshold) {
+            modCount++;
             resize();
         }
         return null;
     }
 
-    private V removeNode(Object key,Object value,boolean matchValue) {
+    private Node<K,V> removeNode(Object key,Object value,boolean matchValue) {
         if(table!=null && table.length>0) {
             int hash = hash(key);
             int index = hash & (table.length-1);
             if(table[index]!=null) {
-                Node<K,V> prevNode = table[index],foundNode=null;
+                Node<K,V> prevNode = table[index],foundNode;
                 if(hash==prevNode.hash && Objects.equals(key,prevNode.key)) {
                     foundNode = prevNode;
                 } else {
@@ -173,14 +558,15 @@ public class MyHashMap<K,V> extends AbstractMap<K,V> implements Map<K,V>{
                     }
                 }
                 if(foundNode!=null && (!matchValue || Objects.equals(value,foundNode.value))) {
-                    V oldValue = foundNode.value;
                     if(prevNode==foundNode) {
                         table[index] = foundNode.next;
                     } else {
                         prevNode.next = foundNode.next;
                     }
                     size--;
-                    return oldValue;
+                    modCount++;
+                    foundNode.next=null;
+                    return foundNode;
                 }
             }
         }
@@ -206,7 +592,7 @@ public class MyHashMap<K,V> extends AbstractMap<K,V> implements Map<K,V>{
 
     private void resize() {
         int oldCap = table==null ? 0: table.length;
-        int newCap = 0;
+        int newCap;
         if(oldCap>0) {
             if(oldCap==MAX_CAP) {
                 return;
@@ -221,6 +607,7 @@ public class MyHashMap<K,V> extends AbstractMap<K,V> implements Map<K,V>{
         float ft = (float)newCap*loadFactor;
         threshold = ft >= (float)Integer.MAX_VALUE ? Integer.MAX_VALUE : (int)ft;
 
+        @SuppressWarnings("unchecked")
         Node<K,V>[] newTable = (Node<K,V>[])new Node[newCap];
         for(int i=0;i<oldCap;i++) {
             if(table[i]!=null) {
@@ -271,7 +658,7 @@ public class MyHashMap<K,V> extends AbstractMap<K,V> implements Map<K,V>{
         return key==null ? 0 : key.hashCode();
     }
 
-    private class Node<K,V> {
+    private static class Node<K,V> implements Entry<K,V>{
         final int hash;
         final K key;
         V value;
@@ -284,6 +671,35 @@ public class MyHashMap<K,V> extends AbstractMap<K,V> implements Map<K,V>{
             this.next = next;
         }
 
-    }
+        @Override
+        public K getKey() {
+            return key;
+        }
 
+        @Override
+        public V getValue() {
+            return value;
+        }
+
+        @Override
+        public V setValue(V value) {
+            V oldValue = this.value;
+            this.value = value;
+            return oldValue;
+        }
+
+        @Override
+        public int hashCode() {
+            return hash ^ hash(value);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if(o instanceof Entry<?,?>) {
+                Entry<?,?> entry = (Entry<?,?>) o;
+                return Objects.equals(key,entry.getKey()) && Objects.equals(value,entry.getValue());
+            }
+            return false;
+        }
+    }
 }
